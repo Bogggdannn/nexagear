da2afbd87a4543e9fd45751b2e0b841b4c83860d
Bogggdannn <b.grishin1@yandex.ru>
2025-04-03 20:45:44 +0500

    половина слайдера готова

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -8,14 +8,19 @@ export function SliderDefault() {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-      };
-    
+    };
+
     return (
         <div className="head-slider">
-            <Slider {...settings}>
+            <Slider className="slider"{...settings}>
                 <div className="slide">
-                    <img src="/images/mouse.jpg" alt="" />
-                    <div className="slide-content"></div>
+                    <img src="/images/mouse.jpg" alt="" className="slide-head-img" />
+                    <div className="slide-content">
+                        <div className="slider-get-now">Get<br/> Now</div>
+                        <div className="slider-information">               
+                            <p className="slider-price">$89,00</p>
+                            <p className="slider-text-content">GForce Gen 3 Consoles</p></div>
+                    </div>
                 </div>
                 <div className="slide">
                     <h3>1</h3>
